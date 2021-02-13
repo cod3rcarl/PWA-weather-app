d2c5a285d7296f88a6dbf6c6adcd86e16f24e283
cod3rcarl <cod3rcarl@gmail.com>
2021-02-13 22:12:24 +0000

    added readme images

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const URL = {
+  key: process.env.REACT_APP_API_KEY,
+  base: "http://api.openweathermap.org/data/2.5/weather",
+};
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL.base, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: URL.key,
+    },
+  });
+  return data;
+};
